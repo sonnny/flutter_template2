deb7c218fea0f7b4a28d2f36224c0f696c8e43de
sonnny <sonnybalut@yahoo.com>
2023-05-04 16:12:28 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+// main.dart
+
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home());}}
+ 
